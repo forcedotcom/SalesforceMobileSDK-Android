a6be5f9b58557b08590e94ecb6bdc40b26c46e2e
Wolfgang Mathurin <wmathurin@salesforce.com>
2011-10-24 16:03:23 -0700

    Integration with salesforce sdk (work in progress - not quite working yet)

--- hybrid/SampleApps/ContactExplorer/ContactExplorer/src/com/salesforce/samples/contactexplorer/ContactExplorerActivity.java
+++ hybrid/SampleApps/ContactExplorer/ContactExplorer/src/com/salesforce/samples/contactexplorer/ContactExplorerActivity.java
@@ -1,14 +1,48 @@
 package com.salesforce.samples.contactexplorer;
 
-import com.phonegap.*;
+import java.util.HashMap;
 
+import org.json.JSONObject;
+
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.phonegap.DroidGap;
+import com.salesforce.androidsdk.auth.AbstractLoginActivity;
+import com.salesforce.androidsdk.rest.ClientManager;
+import com.salesforce.androidsdk.rest.ClientManager.RestClientCallback;
+import com.salesforce.androidsdk.rest.RestClient;
 
 public class ContactExplorerActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        ContactExplorerActivity.super.loadUrl("file:///android_asset/www/index.html");
+        
+        final String accountType = getString(R.string.account_type);
+		new ClientManager(this, accountType).getRestClient(this, new RestClientCallback() {
+			public void authenticatedRestClient(RestClient client) {
+				if (client == null) {
+					// Go back to login screen
+			    	new ClientManager(ContactExplorerActivity.this, accountType).removeAccountAsync(null);
+			    	Intent i = new Intent(AbstractLoginActivity.ACTION_LOGIN);
+			    	i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			    	startActivity(i);
+			    	return;
+				}
+				
+				HashMap<String, String> data = new HashMap<String, String>();
+				data.put("clientId", getString(R.string.oauth_client_id));
+				data.put("loginUrl", getString(R.string.login_url));
+				data.put("apiVersion", getString(R.string.api_version));
+				data.put("authToken", client.getAuthToken());
+				// TODO: also send down instanceUrl and refreshToken
+				
+				String eventJs = "{'data':" + new JSONObject(data).toString() + "}";
+				String jsCall = "onSalesforceOAuthLogin(" + eventJs + ")";
+				sendJavascript(jsCall);
+			}
+		});
     }
 }
---
+++ hybrid/SampleApps/ContactExplorer/ContactExplorer/src/com/salesforce/samples/contactexplorer/LoginActivity.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2011, salesforce.com, inc.
+ * All rights reserved.
+ * Redistribution and use of this software in source and binary forms, with or
+ * without modification, are permitted provided that the following conditions
+ * are met:
+ * - Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * - Neither the name of salesforce.com, inc. nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission of salesforce.com, inc.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.salesforce.samples.contactexplorer;
+
+
+import android.view.View;
+import android.widget.Toast;
+
+import com.salesforce.androidsdk.auth.AbstractLoginActivity;
+
+/**
+ * Activity responsible for login
+ *
+ */
+public class LoginActivity extends AbstractLoginActivity {
+	
+	// Key for login servers properties stored in preferences
+	public static final String SERVER_URL_PREFS_SETTINGS = "server_url_prefs";
+	public static final String SERVER_URL_PREFS_CUSTOM_LABEL = "server_url_custom_label";
+	public static final String SERVER_URL_PREFS_CUSTOM_URL = "server_url_custom_url";
+	public static final String SERVER_URL_PREFS_WHICH_SERVER = "which_server_index";
+	public static final String SERVER_URL_CURRENT_SELECTION = "server_url_current_string";
+	
+	// Request code when calling PickServerActivity
+    public static final int PICK_SERVER_CODE = 10;
+	
+    /**************************************************************************************************
+     *
+     * Implementations for abstract methods of AbstractLoginActivity
+     * 
+     **************************************************************************************************/
+    
+	@Override
+	protected int getLayoutId() {
+		return R.layout.login;
+	}
+
+	@Override
+	protected int getWebViewId() {
+		return R.id.oauth_webview;
+	}
+	
+	@Override
+	protected void showError(Exception exception) {
+		Toast.makeText(this,
+				getString(R.string.generic_error, exception.toString()),
+				Toast.LENGTH_LONG).show();
+	}
+
+
+	@Override
+	protected String getGenericAuthErrorTitle() {
+		return getString(R.string.generic_authentication_error_title);
+	}
+	
+	@Override
+	protected String getGenericAuthErrorBody() {
+		return getString(R.string.generic_authentication_error);
+	}
+	
+	@Override
+	protected String getOAuthClientId() {
+		return getString(R.string.oauth_client_id);
+	}
+
+	@Override
+	protected String getOAuthCallbackUrl() {
+		return getString(R.string.oauth_callback_url);
+	}
+	
+	@Override
+	protected String getApiVersion() {
+		return getString(R.string.api_version);
+	}
+	
+	@Override
+	protected String getLoginServerUrl() {
+		return getString(R.string.login_url);
+	}
+
+	@Override
+	protected String getAccountType() {
+		return getString(R.string.account_type);
+	}
+	
+	
+    /**************************************************************************************************
+     *
+     * Buttons click handlers
+     * 
+     **************************************************************************************************/
+
+	/**
+	 * Called when "Clear cookies" button is clicked.
+	 * Clear cookies and reload login page.
+	 * @param v
+	 */
+	public void onClearCookiesClick(View v) {
+		clearCookies();
+	}
+}
