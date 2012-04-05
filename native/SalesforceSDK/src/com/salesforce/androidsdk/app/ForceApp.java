9e8b08684d2077f9844d749fc15372e3a6604dc4
Wolfgang Mathurin <wmathurin@salesforce.com>
2012-04-05 15:15:51 -0700

    Now producing 1.2 jar

--- native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
@@ -66,7 +66,7 @@ public abstract class ForceApp extends Application {
 	/**
 	 * Current version of this SDK.
 	 */
-    public static final String SDK_VERSION = "1.1.1";
+    public static final String SDK_VERSION = "1.2";
 
 	/**
      * Instance of the ForceApp to use for this process.
