87cc430c2881a6b78c10a324a781a45428430d39
Wolfgang Mathurin <wmathurin@salesforce.com>
2012-05-15 11:45:31 -0700

    Unstable branch: preview of upcoming release

--- native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
@@ -68,7 +68,7 @@ public abstract class ForceApp extends Application {
 	/**
 	 * Current version of this SDK.
 	 */
-    public static final String SDK_VERSION = "1.2";
+    public static final String SDK_VERSION = "1.2.unstable";
 
     /*
      * Last phone version
