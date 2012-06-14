b2500734f3ed64f307bb676f5b99e804a0f1e1b8
Wolfgang Mathurin <wmathurin@salesforce.com>
2012-06-14 15:01:12 -0700

    sf__ prefix for all resource files (and references) - also linking entire directory when possible

--- hybrid/SampleApps/ContactExplorer/src/com/salesforce/samples/contactexplorer/SalesforceRImpl.java
+++ hybrid/SampleApps/ContactExplorer/src/com/salesforce/samples/contactexplorer/SalesforceRImpl.java
@@ -36,12 +36,12 @@ import com.salesforce.androidsdk.ui.SalesforceR;
 public class SalesforceRImpl extends SalesforceR {
 	/* Login */
 	public int stringAccountType() { return R.string.account_type; }
-	public int layoutLogin() {return R.layout.login; }
+	public int layoutLogin() {return R.layout.sf__login; }
 	public int idLoginWebView() {return R.id.sf__oauth_webview; }
 	public int stringGenericError() {return R.string.sf__generic_error; }
 	public int stringGenericAuthenticationErrorTitle() {return R.string.sf__generic_authentication_error_title; } 
 	public int stringGenericAuthenticationErrorBody() {return R.string.sf__generic_authentication_error; }
-	public int menuLogin() { return R.menu.login; }
+	public int menuLogin() { return R.menu.sf__login; }
 	public int idAuthContainer() { return R.id.sf__auth_container; }
 	public int idItemClearCookies() { return R.id.sf__menu_clear_cookies; }
 	public int idItemPickServer() { return R.id.sf__menu_pick_server; }
@@ -52,7 +52,7 @@ public class SalesforceRImpl extends SalesforceR {
 	public int styleTextHostName() { return R.style.Text_HostName; }
 	public int styleTextHostUrl() { return R.style.Text_HostUrl; }
 	/* Passcode */
-	public int layoutPasscode() {return R.layout.passcode; }
+	public int layoutPasscode() {return R.layout.sf__passcode; }
 	public int idPasscodeTitle() {return R.id.sf__passcode_title; }
 	public int idPasscodeError() {return R.id.sf__passcode_error; }
 	public int idPasscodeInstructions() {return R.id.sf__passcode_instructions; }
@@ -74,16 +74,16 @@ public class SalesforceRImpl extends SalesforceR {
 	public int stringServerUrlDefaultCustomUrl() {return R.string.sf__server_url_default_custom_url;}
 	public int stringServerUrlAddTitle() {return R.string.sf__server_url_add_title;}
 	public int stringServerUrlEditTitle() {return R.string.sf__server_url_edit_title;}
-	public int layoutCustomServerUrl() {return R.layout.custom_server_url; }
+	public int layoutCustomServerUrl() {return R.layout.sf__custom_server_url; }
 	public int idApplyButton() {return R.id.sf__apply_button;}
 	public int idCancelButton() {return R.id.sf__cancel_button;}
 	public int stringInvalidServerUrl() {return R.string.sf__invalid_server_url;}
 	public int idServerListGroup() {return R.id.sf__server_list_group; }
-	public int layoutServerPicker() {return R.layout.server_picker; }
+	public int layoutServerPicker() {return R.layout.sf__server_picker; }
 	public int stringAuthLoginProduction() {return R.string.sf__auth_login_production;}
 	public int stringAuthLoginSandbox() {return R.string.sf__auth_login_sandbox;}
-	public int menuClearCustomUrl() {return R.menu.clear_custom_url; }
+	public int menuClearCustomUrl() {return R.menu.sf__clear_custom_url; }
 	public int idMenuClearCustomUrl() {return R.id.sf__menu_clear_custom_url;}
-	public int drawableEditIcon() {return R.drawable.edit_icon; }
+	public int drawableEditIcon() {return R.drawable.sf__edit_icon; }
 	public int idShowCustomUrlEdit() {return R.id.sf__show_custom_url_edit;}
 }
