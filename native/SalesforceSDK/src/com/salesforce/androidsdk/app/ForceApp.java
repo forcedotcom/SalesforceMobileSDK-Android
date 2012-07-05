c621ff81008fcd8d3c33bde7777be22f7ec8ec74
Bharath Hariharan <bhariharan@salesforce.com>
2012-07-05 11:33:18 -0700

    Bugfix for NPE

--- native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
@@ -69,7 +69,7 @@ public abstract class ForceApp extends Application {
 	/**
 	 * Current version of this SDK.
 	 */
-    public static final String SDK_VERSION = "1.2.1";
+    public static final String SDK_VERSION = "1.2.2";
 
     /*
      * Last phone version
