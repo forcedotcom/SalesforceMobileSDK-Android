bdba2da329f27c0c73be9a25842b5d767105a7ec
Simon Fell <fellforce@gmail.com>
2012-07-16 11:01:50 -0700

    consume response when handling a SC_UNAUTHORIZED response.

--- native/SalesforceSDK/src/com/salesforce/androidsdk/rest/RestClient.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/rest/RestClient.java
@@ -232,6 +232,8 @@ public class RestClient {
 			// If we haven't retried already and we have an accessTokeProvider
 			// Then let's try to get a new authToken
 			if (retryInvalidToken && authTokenProvider != null) {
+				// remember to consume this response so the connection can get re-used
+				restResponse.consume();
 				String newAuthToken = authTokenProvider.getNewAuthToken();
 				if (newAuthToken != null) {
 					setAuthToken(newAuthToken);
