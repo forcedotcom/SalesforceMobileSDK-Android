500b8ccaecf70849f77ab41033477b676cea4364
Wolfgang Mathurin <wmathurin@salesforce.com>
2012-07-17 15:37:21 -0700

    v1.2.4

--- native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
@@ -69,7 +69,7 @@ public abstract class ForceApp extends Application {
 	/**
 	 * Current version of this SDK.
 	 */
-    public static final String SDK_VERSION = "1.2.3";
+    public static final String SDK_VERSION = "1.2.4";
 
     /*
      * Last phone version
