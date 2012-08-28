0fdecf91fa31b6637f77f7200d6535429acdb8a5
Wolfgang Mathurin <wmathurin@salesforce.com>
2012-08-27 17:12:50 -0700

    Setting up javascript when gap webview is first created

--- hybrid/ContactExplorerTest/src/com/salesforce/samples/contactexplorer/ContactExplorerTest.java
+++ hybrid/ContactExplorerTest/src/com/salesforce/samples/contactexplorer/ContactExplorerTest.java
@@ -28,9 +28,7 @@ package com.salesforce.samples.contactexplorer;
 
 import org.json.JSONObject;
 
-import android.app.Activity;
 import android.app.Instrumentation;
-import android.app.Instrumentation.ActivityMonitor;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -40,8 +38,8 @@ import android.webkit.WebView;
 import com.salesforce.androidsdk.app.ForceApp;
 import com.salesforce.androidsdk.auth.OAuth2;
 import com.salesforce.androidsdk.ui.LoginActivity;
-import com.salesforce.androidsdk.ui.SalesforceDroidGapActivity;
 import com.salesforce.androidsdk.util.EventsListenerQueue;
+import com.salesforce.androidsdk.util.EventsListenerQueue.BlockForEvent;
 import com.salesforce.androidsdk.util.EventsObservable;
 import com.salesforce.androidsdk.util.EventsObservable.Event;
 import com.salesforce.androidsdk.util.EventsObservable.EventType;
@@ -51,95 +49,91 @@ import com.salesforce.androidsdk.util.EventsObservable.EventType;
  */
 public class ContactExplorerTest extends InstrumentationTestCase {
 
-	private int TIMEOUT = 10000; // ms
+	private int TIMEOUT = 15000; // ms
     private Instrumentation instrumentation;
 	private EventsListenerQueue eq;
-
+	private WebView gapWebView;
+	
+	
     @Override
     public void setUp() throws Exception {
         super.setUp();
 
         instrumentation = getInstrumentation();
-        
         eq = new EventsListenerQueue();
-        // Wait for app initialization to complete
+
+        waitForStartup();
+    	logout();
+    	useSandbox();
+    	prepareBridge();
+    	launchMainActivity();
+    
+    }
+
+	public void testLoginFlow() throws Exception {
+		WebView loginWebView = (WebView) waitForEvent(EventType.AuthWebViewCreateComplete).getData();
+		waitForEvent(EventType.AuthWebViewPageFinished);
+		runJavaScript(loginWebView, "document.login.un.value='w@cs0.com';document.login.password.value='123456';document.login.submit();"); // login
+		waitForEvent(EventType.AuthWebViewPageFinished);
+		//runJavaScript(loginWebView, "document.editPage.oaapprove.click()"); // approve
+		runJavaScript(loginWebView, "document.editPage[6].click()"); // approve
+		waitForEvent(EventType.GapWebViewPageFinished);
+		runJavaScript(gapWebView, "jQuery('#link_fetch_sfdc_contacts').trigger('click')");
+		Event evtWithResponse = waitForEvent(EventType.Other);
+		JSONObject response = new JSONObject((String) evtWithResponse.getData());
+		
+	}
+
+	private void waitForStartup() {
+		// Wait for app initialization to complete
         if (ForceApp.APP == null) {
             eq.waitForEvent(EventType.AppCreateComplete, TIMEOUT);
         }
-    }	
+	}	
 	
-    public void testLoginFlow() throws Exception {
-    	ForceApp.APP.logout(null, false);
-    	waitForEvent(EventType.LogoutComplete);
-    	
-    	useSandbox();
-
-		final Intent intent = new Intent(Intent.ACTION_MAIN);
-    	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    	intent.setClassName(instrumentation.getTargetContext(), ForceApp.APP.getMainActivityClass().getName());
- 
-    	Activity loginActivity = waitForActivity(LoginActivity.class, new Runnable() {
-    		@Override public void run() { instrumentation.startActivitySync(intent); }
-    	});
-    	WebView loginWebView = (WebView) ((LoginActivity) loginActivity).findViewById(ForceApp.APP.getSalesforceR().idLoginWebView());
-    	
-    	waitForEvent(EventType.LoginWebViewPageFinished);
-    	runJavaScript(loginWebView, "document.login.un.value='w@cs0.com';document.login.password.value='123456';document.login.submit();"); // login
-    	waitForEvent(EventType.LoginWebViewPageFinished);
-    	runJavaScript(loginWebView, "document.editPage.oaapprove.click()"); // approve
-
-    	Activity gapActivity = waitForActivity(SalesforceDroidGapActivity.class, new Runnable() {
-    		@Override public void run() { waitForEvent(EventType.GapWebViewPageFinished); }	
-    	});
-    	
-    	WebView gapWebView = ((SalesforceDroidGapActivity) gapActivity).getWebView();
-    	installBridge(gapWebView);
-    	
-    	runJavaScript(gapWebView, "jQuery('#link_fetch_sfdc_contacts').trigger('click')");
-    	Event e = waitForEvent(EventType.Other);
-    	JSONObject response = new JSONObject(e.getData());
-    	
-    	
-    }
+    private void logout() {
+		ForceApp.APP.logout(null, false);
+		waitForEvent(EventType.LogoutComplete);
+	}
 
 	private void useSandbox() {
 		// Our username/password is for a sandbox org
-    	SharedPreferences settings = instrumentation.getTargetContext().getSharedPreferences(
-                LoginActivity.SERVER_URL_PREFS_SETTINGS,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString(LoginActivity.SERVER_URL_CURRENT_SELECTION, OAuth2.SANDBOX_LOGIN_URL);
-        editor.commit();
-	}
-	
-	/**
-	 * Setup a monitor
-	 * Run action
-	 * Wait for activityClass to be the front activity
-	 * Remove monitor
-	 * @return active activity
-	 */
-	private Activity waitForActivity(Class<? extends Activity> activityClass, Runnable action) {
-		ActivityMonitor monitor = instrumentation.addMonitor(activityClass.getName(), null, false);
-		action.run();
-		Activity activity = instrumentation.waitForMonitor(monitor);
-		assertEquals("Wrong activity returned", activityClass.getName(), activity.getClass().getName());
-		instrumentation.removeMonitor(monitor);
-		return activity;
+		SharedPreferences settings = instrumentation.getTargetContext().getSharedPreferences(
+	            LoginActivity.SERVER_URL_PREFS_SETTINGS,
+	            Context.MODE_PRIVATE);
+	    SharedPreferences.Editor editor = settings.edit();
+	    editor.putString(LoginActivity.SERVER_URL_CURRENT_SELECTION, OAuth2.SANDBOX_LOGIN_URL);
+	    editor.commit();
 	}
 
-    private Event waitForEvent(EventType evt) {
+	private void launchMainActivity() {
+		final Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.setClassName(instrumentation.getTargetContext(), ForceApp.APP.getMainActivityClass().getName());
+		instrumentation.startActivitySync(intent);
+	}
+
+	private void prepareBridge() {
+			// Bridge must be installed before anything gets loaded in the webview
+			eq.registerBlock(new BlockForEvent(EventType.GapWebViewCreateComplete) {
+				@Override
+				public void run(Event evt) {
+					gapWebView = (WebView) evt.getData();
+					gapWebView.addJavascriptInterface(new Object() {
+						@SuppressWarnings("unused")
+						public void notifyEvent(String msg) {
+							EventsObservable.get()
+									.notifyEvent(EventType.Other, msg);
+						}
+					}, "containerObserver");
+				}
+			});
+	 }
+
+	private Event waitForEvent(EventType evt) {
     	return eq.waitForEvent(evt, TIMEOUT);
     }
   
-    
-    private void installBridge(WebView webView) {
-    	webView.addJavascriptInterface(new Object() {
-    		public void notifyEvent(String msg) {
-    			EventsObservable.get().notifyEvent(new Event(EventType.Other, msg));
-    		}
-    	}, "containerObserver");
-    }
     
     private void runJavaScript(final WebView webView, final String js) {
     	try {
