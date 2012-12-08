33f278224a763e34824559ab697943cfc2582351
Wolfgang Mathurin <wmathurin@salesforce.com>
2012-12-07 18:11:16 -0800

    Unstable20 should be for version 2.0.unstable

--- native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/app/ForceApp.java
@@ -63,7 +63,7 @@ public abstract class ForceApp extends Application implements AccountRemoved {
     /**
      * Current version of this SDK.
      */
-    public static final String SDK_VERSION = "1.5.unstable";
+    public static final String SDK_VERSION = "2.0.unstable";
 
     /**
      * Last phone version.
