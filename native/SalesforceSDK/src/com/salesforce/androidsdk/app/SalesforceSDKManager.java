336f1217042f151b1ad8a255221105842426abd9
Bharath Hariharan <bhariharan@salesforce.com>
2013-08-22 16:02:58 -0700

    2.0.2 patch release with Android 4.3 support

--- native/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.java
@@ -71,7 +71,7 @@ public class SalesforceSDKManager implements AccountRemoved {
     /**
      * Current version of this SDK.
      */
-    public static final String SDK_VERSION = "2.0.1";
+    public static final String SDK_VERSION = "2.0.2";
 
     /**
      * Last phone version.
