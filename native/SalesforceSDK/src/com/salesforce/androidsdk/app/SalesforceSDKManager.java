fadd5851310e20198a4e6d0017c128e4642986f4
Bharath Hariharan <bhariharan@salesforce.com>
2013-11-06 15:48:13 -0800

    Version number changes

--- native/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.java
+++ native/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.java
@@ -79,7 +79,7 @@ public class SalesforceSDKManager implements AccountRemoved {
     /**
      * Current version of this SDK.
      */
-    public static final String SDK_VERSION = "2.1.0";
+    public static final String SDK_VERSION = "2.2.0.unstable";
 
     /**
      * Last phone version.
