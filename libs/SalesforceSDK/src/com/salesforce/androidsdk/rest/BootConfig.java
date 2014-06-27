fee802f9651ff7b7e24976e59ac9d5a472789c6a
Wolfgang Mathurin <wmathurin@salesforce.com>
2014-06-27 14:10:38 -0700

    Returning bootconfig as part of sdk info

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/rest/BootConfig.java
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/rest/BootConfig.java
@@ -27,6 +27,7 @@
 package com.salesforce.androidsdk.rest;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.json.JSONArray;
@@ -90,6 +91,29 @@ public class BootConfig {
 			}
 		}
 		return INSTANCE;
+	}
+	
+    /**
+     * @return boot config as JSONObject
+	 * @throws JSONException
+	 */
+	public JSONObject asJSON() throws JSONException
+	{
+		JSONObject config = new JSONObject();
+
+		config.put(REMOTE_ACCESS_CONSUMER_KEY, remoteAccessConsumerKey);
+		config.put(OAUTH_REDIRECT_URI, oauthRedirectURI);
+		config.put(OAUTH_SCOPES, new JSONArray(Arrays.asList(oauthScopes)));
+        config.put(IS_LOCAL, isLocal);
+        config.put(START_PAGE, startPage);
+        config.put(ERROR_PAGE, errorPage);
+
+        if (pushNotificationClientId != null) 
+            config.put(PUSH_NOTIFICATION_CLIENT_ID, pushNotificationClientId);
+        config.put(SHOULD_AUTHENTICATE, shouldAuthenticate);
+        config.put(ATTEMPT_OFFLINE_LOAD, attemptOfflineLoad);
+
+        return config;
 	}
 
 	/**
