cbea15b9e0d59343b3614d8687cec7940b7963c5
Jason <jwatt@salesforce.com>
2014-10-03 14:08:08 -0700

    Trying to compile

---
+++ worker_setup.sh
@@ -0,0 +1,13 @@
+cd libs/SalesforceSDK
+
+~/android-sdk-linux/tools/android update project -p .
+
+cd ../../external/cordova/framework
+
+~/android-sdk-linux/tools/android update project -p .
+
+cd ../../../native/SampleApps/FileExplorer
+
+~/android-sdk-linux/tools/android update project -p .
+
+ant debug
