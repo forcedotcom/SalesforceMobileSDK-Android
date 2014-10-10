cb26880f93a97c9730395bde6ef28af8f93d01e8
Jay Moorthi <jay@tddium.com>
2014-10-09 17:40:23 -0700

    Update setup scripts

--- pre_setup.sh
+++ pre_setup.sh
@@ -7,6 +7,15 @@ cd ~
 
 curl http://dl.google.com/android/android-sdk_r23.0.2-linux.tgz | tar zxv
 
-yes | $ANDROID_SDK/tools/android  update sdk --filter platform-tools,build-tools-19.0.3,sysimg-17,android-17,extra-android-support --no-ui --force
-yes no | $ANDROID_SDK/tools/android create avd --force -n test -t android-17 --abi armeabi-v7a
+#expect -c '
+#set timeout -1   ;
+#spawn ~/android-sdk-linux/tools/android update sdk -u; 
+#expect { 
+#    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+#    eof
+#}
+#'
+
+echo y | $ANDROID_SDK/tools/android  update sdk --filter platform-tools,build-tools-19.1.0,sys-img-armeabi-v7a-android-17,sysimg-17,android-17,extra-android-support --no-ui --force --all
+echo no | $ANDROID_SDK/tools/android create avd --force -n test -t android-17 --abi armeabi-v7a
 
