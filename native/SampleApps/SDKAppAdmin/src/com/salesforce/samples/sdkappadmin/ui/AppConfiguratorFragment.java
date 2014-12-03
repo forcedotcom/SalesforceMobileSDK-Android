d04deed53703a543775f7b5cc1620c9c2e1bfa25
Wolfgang Mathurin <wmathurin@salesforce.com>
2014-12-02 17:45:11 -0800

    Capturing login host as an app restriction

--- native/SampleApps/SDKAppAdmin/src/com/salesforce/samples/sdkappadmin/ui/AppConfiguratorFragment.java
+++ native/SampleApps/SDKAppAdmin/src/com/salesforce/samples/sdkappadmin/ui/AppConfiguratorFragment.java
@@ -42,8 +42,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -54,10 +53,14 @@ import com.salesforce.samples.sdkappadmin.R;
  * This fragment provides UI and functionality to configure target application
  * sample.
  */
-public class AppConfiguratorFragment extends Fragment implements View.OnClickListener,
-        CompoundButton.OnCheckedChangeListener {
+public class AppConfiguratorFragment extends Fragment implements View.OnClickListener {
 
-    /**
+	/**
+	 * Default login host
+	 */
+    private static final String DEFAULT_LOGIN_HOST = "https://login.salesforce.com";
+
+	/**
      * Package name of the target app sample.
      */
     private static final String PACKAGE_NAME_TARGET_APP
@@ -66,23 +69,19 @@ public class AppConfiguratorFragment extends Fragment implements View.OnClickLis
     /**
      * Key for {@link SharedPreferences}
      */
-    private static final String PREFS_KEY = "AppRestrictionEnforcerFragment";
+    private static final String PREFS_KEY = "AppConfiguratorFragment";
 
     /**
-     * Key for the boolean restriction in AppRestrictionSchema.
+     * Key for the string restriction in target app.
      */
-    private static final String RESTRICTION_KEY_SAY_HELLO = "can_say_hello";
+    private static final String CONFIGURATION_LOGIN_HOST = "loginHost";
 
-    /**
-     * Default boolean value for "can_say_hello" restriction. The actual value is loaded in
-     * {@link #loadRestrictions(android.app.Activity)}.
-     */
-    private boolean mDefaultValueRestrictionSayHello;
 
     // UI Components
     private TextView mTextStatus;
     private Button mButtonUnhide;
-    private Switch mSwitchSayHello;
+    private Button mButtonSave;
+    private EditText mLoginHost;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -93,10 +92,11 @@ public class AppConfiguratorFragment extends Fragment implements View.OnClickLis
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         mTextStatus = (TextView) view.findViewById(R.id.status);
+        mLoginHost = (EditText) view.findViewById(R.id.login_host);
         mButtonUnhide = (Button) view.findViewById(R.id.unhide);
-        mSwitchSayHello = (Switch) view.findViewById(R.id.say_hello);
+        mButtonSave = (Button) view.findViewById(R.id.save);
         mButtonUnhide.setOnClickListener(this);
-        mSwitchSayHello.setOnCheckedChangeListener(this);
+        mButtonSave.setOnClickListener(this);
     }
 
     @Override
@@ -108,24 +108,16 @@ public class AppConfiguratorFragment extends Fragment implements View.OnClickLis
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.unhide: {
+            case R.id.unhide: 
                 unhideApp(getActivity());
                 break;
-            }
-        }
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-        switch (compoundButton.getId()) {
-            case R.id.say_hello: {
-                allowSayHello(getActivity(), checked);
+            case R.id.save: 
+            	saveLoginHost(getActivity(), mLoginHost.getText().toString());
                 break;
-            }
         }
     }
 
-    /**
+	/**
      * Updates the UI components according to the current status of AppRestrictionSchema and its
      * restriction.
      *
@@ -143,32 +135,29 @@ public class AppConfiguratorFragment extends Fragment implements View.OnClickLis
                         AppConfiguratorAdminReceiver.getComponentName(activity),
                         PACKAGE_NAME_TARGET_APP)) {
                     // The app is ready
-                    loadRestrictions(activity);
                     mTextStatus.setVisibility(View.GONE);
                     mButtonUnhide.setVisibility(View.GONE);
-                    mSwitchSayHello.setVisibility(View.VISIBLE);
-                    mSwitchSayHello.setOnCheckedChangeListener(null);
-                    mSwitchSayHello.setChecked(canSayHello(activity));
-                    mSwitchSayHello.setOnCheckedChangeListener(this);
+                    mLoginHost.setVisibility(View.VISIBLE);
+                    mLoginHost.setText(getLoginHost(activity));
                 } else {
                     // The app is installed but hidden in this profile
                     mTextStatus.setText(R.string.status_not_activated);
                     mTextStatus.setVisibility(View.VISIBLE);
                     mButtonUnhide.setVisibility(View.VISIBLE);
-                    mSwitchSayHello.setVisibility(View.GONE);
+                    mLoginHost.setVisibility(View.GONE);
                 }
             } else {
                 // Need to reinstall the sample app
                 mTextStatus.setText(R.string.status_need_reinstall);
                 mTextStatus.setVisibility(View.VISIBLE);
                 mButtonUnhide.setVisibility(View.GONE);
-                mSwitchSayHello.setVisibility(View.GONE);
+                mLoginHost.setVisibility(View.GONE);
             }
         } catch (PackageManager.NameNotFoundException e) {
             mTextStatus.setText(R.string.status_not_installed);
             mTextStatus.setVisibility(View.VISIBLE);
             mButtonUnhide.setVisibility(View.GONE);
-            mSwitchSayHello.setVisibility(View.GONE);
+            mLoginHost.setVisibility(View.GONE);
         }
     }
 
@@ -188,56 +177,52 @@ public class AppConfiguratorFragment extends Fragment implements View.OnClickLis
     }
 
     /**
-     * Loads the restrictions for the AppRestrictionSchema sample. In this implementation, we just
-     * read the default value for the "can_say_hello" restriction.
-     *
-     * @param activity The activity
+     * Return login host from app restrictions
+     * 
+     * @param activity
      */
-    private void loadRestrictions(Activity activity) {
+    private String getLoginHostFromAppRestrictions(Activity activity) {
         RestrictionsManager restrictionsManager =
                 (RestrictionsManager) activity.getSystemService(Context.RESTRICTIONS_SERVICE);
         List<RestrictionEntry> restrictions =
                 restrictionsManager.getManifestRestrictions(PACKAGE_NAME_TARGET_APP);
         for (RestrictionEntry restriction : restrictions) {
-            if (RESTRICTION_KEY_SAY_HELLO.equals(restriction.getKey())) {
-                mDefaultValueRestrictionSayHello = restriction.getSelectedState();
+            if (CONFIGURATION_LOGIN_HOST.equals(restriction.getKey())) {
+                return restriction.getSelectedString();
             }
         }
+        return DEFAULT_LOGIN_HOST;
     }
 
     /**
-     * Returns whether the AppRestrictionSchema is currently allowed to say hello to its user. Note
-     * that a profile/device owner needs to remember each restriction value on its own.
-     *
-     * @param activity The activity
-     * @return True if the AppRestrictionSchema is allowed to say hello
+     * Return login host from preferences or if not found from app restrictions
+     * 
+     * @param activity
      */
-    private boolean canSayHello(Activity activity) {
+    private String getLoginHost(Activity activity) {
         SharedPreferences prefs = activity.getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE);
-        return prefs.getBoolean(RESTRICTION_KEY_SAY_HELLO, mDefaultValueRestrictionSayHello);
+        return prefs.getString(CONFIGURATION_LOGIN_HOST, getLoginHostFromAppRestrictions(activity));
     }
 
     /**
-     * Sets the value for the "cay_say_hello" restriction of AppRestrictionSchema.
-     *
-     * @param activity The activity
-     * @param allow    The value to be set for the restriction.
+     * Set login host in preferences and in application restriction
+     * 
+     * @param activity
+     * @param loginHost
      */
-    private void allowSayHello(Activity activity, boolean allow) {
+    private void saveLoginHost(Activity activity, String loginHost) {
         DevicePolicyManager devicePolicyManager
                 = (DevicePolicyManager) activity.getSystemService(Context.DEVICE_POLICY_SERVICE);
         Bundle restrictions = new Bundle();
-        restrictions.putBoolean(RESTRICTION_KEY_SAY_HELLO, allow);
+        restrictions.putString(CONFIGURATION_LOGIN_HOST, loginHost);
         devicePolicyManager.setApplicationRestrictions(
                 AppConfiguratorAdminReceiver.getComponentName(activity),
                 PACKAGE_NAME_TARGET_APP, restrictions);
         // The profile/device owner needs to remember the current state of restrictions on its own
         activity.getSharedPreferences(PREFS_KEY, Context.MODE_PRIVATE)
                 .edit()
-                .putBoolean(RESTRICTION_KEY_SAY_HELLO, allow)
+                .putString(CONFIGURATION_LOGIN_HOST, loginHost)
                 .apply();
-        Toast.makeText(activity, allow ? R.string.allowed : R.string.disallowed,
-                Toast.LENGTH_SHORT).show();
     }
 
 }
