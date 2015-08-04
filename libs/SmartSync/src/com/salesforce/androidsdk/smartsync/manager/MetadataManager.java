7233fc6c69dedc14276c7bcaf3c30c296306dd43
Bharath Hariharan <bhariharan@salesforce.com>
2015-08-04 15:57:47 -0700

    MRU bug fix

--- libs/SmartSync/src/com/salesforce/androidsdk/smartsync/manager/MetadataManager.java
+++ libs/SmartSync/src/com/salesforce/androidsdk/smartsync/manager/MetadataManager.java
@@ -1092,12 +1092,7 @@ public class MetadataManager {
             }
             String whereClause;
             if (objContainsLastViewedDate) {
-
-            	/*
-            	 * TODO: This should be replaced with 'using SCOPE MRU'
-            	 * in 'v32.0'.
-            	 */
-                queryBuilder.from(String.format("%s using MRU", objectTypeName));
+                queryBuilder.from(String.format("%s using SCOPE MRU", objectTypeName));
                 whereClause = "LastViewedDate != NULL";
                 queryBuilder.orderBy("LastViewedDate DESC");
                 queryBuilder.limit(limit);
