6013ac71bce67e83afd5d862ff8e1cb3fde5e780
Bharath Hariharan <bhariharan@salesforce.com>
2016-04-04 15:16:58 -0700

    Renaming API

--- libs/SmartSync/src/com/salesforce/androidsdk/smartsync/manager/SyncManager.java
+++ libs/SmartSync/src/com/salesforce/androidsdk/smartsync/manager/SyncManager.java
@@ -292,7 +292,7 @@ public class SyncManager {
      *
      * @param syncId Sync ID.
      */
-    public void reSyncRemoteUpdates(long syncId) throws JSONException {
+    public void reSyncCleanGhosts(long syncId) throws JSONException {
         if (runningSyncIds.contains(syncId)) {
             throw new SmartSyncException("Cannot run syncRemoteDeletes:" + syncId + ": still running");
         }
