2f3c5712081f5beeed5514ba5afbf49d160f7496
Qingchun Ren <qren@salesforce.com>
2016-07-26 11:51:33 -0700

    address more comments

--- libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/util/TestDataCleanupHelper.java
+++ libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/util/TestDataCleanupHelper.java
@@ -24,7 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package com.salesforce.androidsdk.helpers;
+package com.salesforce.androidsdk.util;
 
 import android.test.InstrumentationTestCase;
 
