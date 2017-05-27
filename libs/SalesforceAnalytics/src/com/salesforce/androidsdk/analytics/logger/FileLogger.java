139028821ea2b010c32da98af60b2718592e69ab
Bharath Hariharan <bhariharan@salesforce.com>
2017-05-27 11:58:23 -0700

    Adding implementation for file logger

--- libs/SalesforceAnalytics/src/com/salesforce/androidsdk/analytics/logger/FileLogger.java
+++ libs/SalesforceAnalytics/src/com/salesforce/androidsdk/analytics/logger/FileLogger.java
@@ -27,8 +27,15 @@
 package com.salesforce.androidsdk.analytics.logger;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.squareup.tape.ObjectQueue;
+import com.squareup.tape.QueueFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * A simple file logger that works with SalesforceLogger to write log entries to a file.
@@ -38,20 +45,111 @@ import java.io.File;
 public class FileLogger {
 
     private static final String LOG_SUFFIX = "_log";
+    private static final String UTF8 = "UTF-8";
+    private static final String TAG = "FileLogger";
+    private static final int MAX_SIZE = 1000;
 
-    private String componentName;
-    private Context context;
-    private File file;
+    private QueueFile file;
+    private ObjectQueue<String> queue;
+    private int maxSize;
 
     /**
      * Parameterized constructor.
      *
      * @param context Context.
      * @param componentName Component name.
+     * @throws IOException If file initialization was unsuccessful.
      */
-    public FileLogger(Context context, String componentName) {
-        this.componentName = componentName;
-        this.context = context;
-        file = new File(context.getFilesDir(), componentName + LOG_SUFFIX);
+    public FileLogger(Context context, String componentName) throws IOException {
+        final File filename = new File(context.getFilesDir(), componentName + LOG_SUFFIX);
+        file = new QueueFile(filename);
+        maxSize = MAX_SIZE;
+    }
+
+    /**
+     * Flushes the log file and resets it to its original state.
+     *
+     * @throws IOException If the operation failed.
+     */
+    public void flushLog() throws IOException {
+        try {
+            file.clear();
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to flush log file", e);
+        }
+    }
+
+    /**
+     * Returns the number of log lines stored in this file.
+     *
+     * @return Number of stored log lines.
+     */
+    public int getSize() {
+        return file.size();
+    }
+
+    /**
+     * Returns the maximum number of log lines that can be stored in this file.
+     *
+     * @return Maximum number of log lines.
+     */
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    /**
+     * Sets the maximum number of log lines that can be stored in this file.
+     *
+     * @param size Maximum number of log lines.
+     */
+    public synchronized void setMaxSize(int size) {
+        maxSize = size;
+    }
+
+    /**
+     * Writes a log line to the file.
+     *
+     * @param logLine Log line.
+     */
+    public void addLogLine(String logLine) {
+        if (TextUtils.isEmpty(logLine)) {
+            return;
+        }
+        try {
+            while (getSize() >= maxSize) {
+                file.remove();
+            }
+            file.add(logLine.getBytes(UTF8));
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to write log line", e);
+        }
+    }
+
+    /**
+     * Writes a list of log lines to the file.
+     *
+     * @param logLines Log lines.
+     */
+    public void addLogLines(List<String> logLines) {
+        if (logLines == null || logLines.size() == 0) {
+            return;
+        }
+        for (final String logLine : logLines) {
+            addLogLine(logLine);
+        }
+    }
+
+    /**
+     * Writes an array of log lines to the file.
+     *
+     * @param logLines Log lines.
+     */
+    public void addLogLines(String[] logLines) {
+        if (logLines == null || logLines.length == 0) {
+            return;
+        }
+        for (final String logLine : logLines) {
+            addLogLine(logLine);
+        }
     }
 }
