12857c22eda23528dc6fb39f2f83d323bc0ae8cb
Wolfgang Mathurin <wmathurin@salesforce.com>
2017-08-31 17:32:40 -0700

    Revert "Merge pull request #1484 from bhariharan/dev"
    This reverts commit 491c8da94a69b6b33d44afd7082f7a9d037bcf24, reversing
    changes made to ea87dc51eddd01f403ba639dac424a8dc1688451.

--- libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/accounts/UserAccountManagerTest.java
+++ libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/accounts/UserAccountManagerTest.java
@@ -70,13 +70,12 @@ public class UserAccountManagerTest extends InstrumentationTestCase {
         }
         userAccMgr = SalesforceSDKManager.getInstance().getUserAccountManager();
         loginOptions = new LoginOptions(ClientManagerTest.TEST_LOGIN_URL,
-        		ClientManagerTest.TEST_CALLBACK_URL,
-        		ClientManagerTest.TEST_CLIENT_ID,
-				ClientManagerTest.TEST_SCOPES,
-                null);
+        		ClientManagerTest.TEST_PASSCODE_HASH, ClientManagerTest.TEST_CALLBACK_URL,
+        		ClientManagerTest.TEST_CLIENT_ID, ClientManagerTest.TEST_SCOPES);
         clientManager = new ClientManager(targetContext,
         		ClientManagerTest.TEST_ACCOUNT_TYPE, loginOptions, true);
         accMgr = clientManager.getAccountManager();
+        SalesforceSDKManager.getInstance().getPasscodeManager().setPasscodeHash(ClientManagerTest.TEST_PASSCODE_HASH);
     }
 
     @Override
@@ -114,6 +113,7 @@ public class UserAccountManagerTest extends InstrumentationTestCase {
     	List<UserAccount> users = userAccMgr.getAuthenticatedUsers();
     	assertNull("There should be no authenticated users", users);
     	createTestAccount();
+    	SalesforceSDKManager.getInstance().getPasscodeManager().setPasscodeHash(ClientManagerTest.TEST_PASSCODE_HASH);
     	users = userAccMgr.getAuthenticatedUsers();
     	assertEquals("There should be 1 authenticated user", 1, users.size());
     	final UserAccount curUser = userAccMgr.getCurrentUser();
@@ -234,7 +234,7 @@ public class UserAccountManagerTest extends InstrumentationTestCase {
         		ClientManagerTest.TEST_AUTH_TOKEN, ClientManagerTest.TEST_INSTANCE_URL,
         		ClientManagerTest.TEST_LOGIN_URL, ClientManagerTest.TEST_IDENTITY_URL,
         		ClientManagerTest.TEST_CLIENT_ID, ClientManagerTest.TEST_ORG_ID,
-        		ClientManagerTest.TEST_USER_ID, null, null, null, null, null, null, null, null, null, null);
+        		ClientManagerTest.TEST_USER_ID, ClientManagerTest.TEST_PASSCODE_HASH);
     }
 
     /**
@@ -248,6 +248,6 @@ public class UserAccountManagerTest extends InstrumentationTestCase {
         		ClientManagerTest.TEST_AUTH_TOKEN, ClientManagerTest.TEST_INSTANCE_URL,
         		ClientManagerTest.TEST_LOGIN_URL, ClientManagerTest.TEST_IDENTITY_URL,
         		ClientManagerTest.TEST_CLIENT_ID, ClientManagerTest.TEST_ORG_ID_2,
-        		ClientManagerTest.TEST_USER_ID_2, null, null, null, null, null, null, null, null, null, null);
+        		ClientManagerTest.TEST_USER_ID_2, ClientManagerTest.TEST_PASSCODE_HASH);
     }
 }
