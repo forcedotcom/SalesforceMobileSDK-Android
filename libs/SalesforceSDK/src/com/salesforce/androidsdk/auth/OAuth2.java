3daf121aa701e65fbdc83c0dd39155cc11e360f2
Bharath Hariharan <bhariharan@salesforce.com>
2017-10-17 18:24:50 -0700

    Fixing param bug

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/auth/OAuth2.java
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/auth/OAuth2.java
@@ -265,7 +265,7 @@ public class OAuth2 {
             sb.append(AND).append(SCOPE).append(EQUAL).append(Uri.encode(computeScopeParameter(scopes)));
         }
         sb.append(AND).append(REDIRECT_URI).append(EQUAL).append(callbackUrl);
-        sb.append(AND).append(CODE_CHALLENGE).append(EQUAL).append(callbackUrl);
+        sb.append(AND).append(CODE_CHALLENGE).append(EQUAL).append(codeChallenge);
         return sb.toString();
     }
 
