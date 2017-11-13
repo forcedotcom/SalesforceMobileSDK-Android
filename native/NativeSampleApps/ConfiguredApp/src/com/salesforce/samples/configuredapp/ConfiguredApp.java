73faba0fa62ee62d42e88905ce2eb4c68bd9d5b0
Bharath Hariharan <bhariharan@salesforce.com>
2017-11-13 14:40:39 -0800

    Removing setter for IDP login flow

--- native/NativeSampleApps/ConfiguredApp/src/com/salesforce/samples/configuredapp/ConfiguredApp.java
+++ native/NativeSampleApps/ConfiguredApp/src/com/salesforce/samples/configuredapp/ConfiguredApp.java
@@ -42,11 +42,10 @@ public class ConfiguredApp extends Application {
 		SalesforceSDKManager.initNative(getApplicationContext(), null, MainActivity.class);
 
 		/*
-         * Uncomment the following lines to enable IDP login flow. This will allow the user to
+         * Uncomment the following line to enable IDP login flow. This will allow the user to
          * either authenticate using the current app or use a designated IDP app for login.
          * Replace 'idpAppURIScheme' with the URI scheme of the IDP app meant to be used.
          */
-		// SalesforceSDKManager.getInstance().setIDPLoginFlowEnabled(true);
         // SalesforceSDKManager.getInstance().setIDPAppURIScheme(idpAppURIScheme);
 
 		/*
