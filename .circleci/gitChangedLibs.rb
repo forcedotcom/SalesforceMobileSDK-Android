b9116a15c1cacd6f76c0d2eb2d31038ef14559ef
brandonpage <bpage1@salesforce.com>
2018-01-03 15:35:45 -0800

    Fix pr check

--- .circleci/gitChangedLibs.rb
+++ .circleci/gitChangedLibs.rb
@@ -3,27 +3,25 @@ require 'set'
 
 libsTopoSorted = ["SalesforceAnalytics", "SalesforceSDK", "SmartStore", "SmartSync", "SalesforceHybrid", "SalesforceReact"]
 
-if not ENV["CIRCLE_PULL_REQUEST"].nil? {
+if not ENV["CIRCLE_PULL_REQUEST"].nil?
   $GITPRAPI = "https://api.github.com/repos/%s/SalesforceMobileSDK-android/pulls/%s/files"
 
   prFilesAPI = $GITPRAPI % [ENV["CIRCLE_PROJECT_USERNAME"], ENV["CIRCLE_PR_NUMBER"]]
-  print "pr files: %s" % prFilesAPI
-  #pullfiles = `#{"curl %s" % [prFilesAPI]}`
-  #prfiles = JSON.parse(pullfiles)
+  pullfiles = `#{"curl %s" % [prFilesAPI]}`
+  prfiles = JSON.parse(pullfiles)
 
-  #libsModified = Set.new
-  #for prfile in prfiles
-  #  path = prfile["filename"]
-  #  for lib in libsTopoSorted
-  #    if path.include? lib
-  #      libsModified.add(lib)
-  #    end
-  #  end
-  #end
+  libsModified = Set.new
+  for prfile in prfiles
+    path = prfile["filename"]
+    for lib in libsTopoSorted
+      if path.include? lib
+        libsModified.add(lib)
+      end
+    end
+  end
 
   # Print so the bash in the CircleCI yml can get the Libs to run
-  #print libsModified.to_a.join(",")
-}
+  print libsModified.to_a.join(",")
 else
   print libsTopoSorted.join(",")
 end
