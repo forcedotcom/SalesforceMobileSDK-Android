fe576b0abe327216aa6379ff56ed03e728ffb174
brandonpage <bpage1@salesforce.com>
2018-01-05 07:39:00 -0800

    Temp fix for packager nightly.

--- .circleci/ci-helper.sh
+++ .circleci/ci-helper.sh
@@ -37,10 +37,10 @@ function waitForAVD {
         local bootanim=""
         export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
         until [[ "$bootanim" =~ "stopped" ]]; do
+            sleep 5
             bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
             echo "emulator status=$bootanim"
         done
-        sleep 30
         echo "Device Booted"
     else
         echo "No need to start an emulator to test ${CURRENT_LIB} for this PR."
