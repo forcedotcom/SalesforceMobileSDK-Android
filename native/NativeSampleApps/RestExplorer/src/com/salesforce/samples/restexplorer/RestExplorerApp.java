6e8c74cbf4d0174202a50c3b4d6ed97393e193a4
Bharath Hariharan <bhariharan@salesforce.com>
2018-05-31 13:37:39 -0700

    Remove static method of forcing browser-based auth

--- native/NativeSampleApps/RestExplorer/src/com/salesforce/samples/restexplorer/RestExplorerApp.java
+++ native/NativeSampleApps/RestExplorer/src/com/salesforce/samples/restexplorer/RestExplorerApp.java
@@ -48,13 +48,6 @@ public class RestExplorerApp extends Application {
         // SalesforceSDKManager.getInstance().setIDPAppURIScheme(idpAppURIScheme);
 
 		/*
-         * Uncomment the following line to enable browser based login. This will use a
-         * Chrome custom tab to login instead of the default WebView. You will also need
-         * to uncomment a few lines of code in SalesforceSDK library project's AndroidManifest.xml.
-         */
-		// SalesforceSDKManager.getInstance().setBrowserLoginEnabled(true);
-
-		/*
 		 * Un-comment the line below to enable push notifications in this app.
 		 * Replace 'pnInterface' with your implementation of 'PushNotificationInterface'.
 		 * Add your Google package ID in 'bootonfig.xml', as the value
