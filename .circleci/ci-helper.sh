088ebe20ef215bd750955d3fe25a3b287cd4b8de
Bharath Hariharan <bhariharan@salesforce.com>
2018-07-09 16:52:35 -0700

    Enhancements to test configs

--- .circleci/ci-helper.sh
+++ .circleci/ci-helper.sh
@@ -5,7 +5,7 @@ function envSetup {
     sudo apt-get update
     sudo apt-get install libqt5widgets5
     sudo npm install -g shelljs@0.7.0
-    sudo npm install -g cordova@7.0.0
+    sudo npm install -g cordova@8.0.0
     cordova telemetry off
 
     ./install.sh
