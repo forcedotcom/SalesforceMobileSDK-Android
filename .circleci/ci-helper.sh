7b593eacd21e37050933cf24058909e00f7597b6
brandonpage <bpage1@salesforce.com>
2018-07-13 15:46:20 -0700

    try to capture logcat

--- .circleci/ci-helper.sh
+++ .circleci/ci-helper.sh
@@ -35,14 +35,14 @@ function printTestsToRun {
 }
 
 function startAVD {
-    if ([ -n "$NIGHTLY_TEST" ] || ([ -n "$CIRCLE_PULL_REQUEST" ] && [[ ${LIBS_TO_TEST} == *"${CURRENT_LIB}"* ]])); then
+    #if ([ -n "$NIGHTLY_TEST" ] || ([ -n "$CIRCLE_PULL_REQUEST" ] && [[ ${LIBS_TO_TEST} == *"${CURRENT_LIB}"* ]])); then
         export LD_LIBRARY_PATH=${ANDROID_HOME}/emulator/lib64:${ANDROID_HOME}/emulator/lib64/qt/lib
         echo "y" | sdkmanager "system-images;android-22;default;armeabi-v7a"
         echo "no" | avdmanager create avd -n test22 -k "system-images;android-22;default;armeabi-v7a"
         emulator64-arm -avd test22 -noaudio -no-window -accel on
-    else
-        echo "No need to start an emulator to test ${CURRENT_LIB} for this PR."
-    fi
+    #else
+    #    echo "No need to start an emulator to test ${CURRENT_LIB} for this PR."
+    #fi
 }
 
 function restartAVD {
@@ -53,7 +53,7 @@ function restartAVD {
 function waitForAVD {
     set +e
 
-    if ([ -n "$NIGHTLY_TEST" ] || ([ -n "$CIRCLE_PULL_REQUEST" ] && [[ ${LIBS_TO_TEST} == *"${CURRENT_LIB}"* ]])); then
+    #if ([ -n "$NIGHTLY_TEST" ] || ([ -n "$CIRCLE_PULL_REQUEST" ] && [[ ${LIBS_TO_TEST} == *"${CURRENT_LIB}"* ]])); then
         local bootanim=""
         export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
         until [[ "$bootanim" =~ "stopped" ]]; do
@@ -65,9 +65,9 @@ function waitForAVD {
         # unlock the emulator screen
         adb shell input keyevent 82
         echo "Device Booted"
-    else
-        echo "No need to start an emulator to test ${CURRENT_LIB} for this PR."
-    fi
+    #else
+    #    echo "No need to start an emulator to test ${CURRENT_LIB} for this PR."
+    #fi
 }
 
 function runTests {
