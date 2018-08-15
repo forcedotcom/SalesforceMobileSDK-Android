5354ff1bada30e9ef35521f244357a4bc251bfa5
brandonpage <bpage1@salesforce.com>
2018-08-15 01:00:33 -0700

    Fix Danger

--- .circleci/ci-helper.sh
+++ .circleci/ci-helper.sh
@@ -64,12 +64,12 @@ function runTests {
 function runDanger {
     if [[ $CIRCLE_BRANCH == *"pull"* ]]; then
         if [ -z "${CURRENT_LIB}" ]; then
-            DANGER_GITHUB_API_TOKEN="5d42eadf98c58c9c4f60""7fcfc72cee4c7ef1486b" danger --dangerfile=.circleci/Dangerfile_PR.rb --danger_id=PR-Check --verbose
+            DANGER_GITHUB_API_TOKEN="d2d9579980154e199b14""ba8173aa935088d30d2a" danger --dangerfile=.circleci/Dangerfile_PR.rb --danger_id=PR-Check --verbose
         else
             if ls libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/*.xml 1> /dev/null 2>&1; then
                 mv libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/*.xml libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/test-results.xml
             fi
-            DANGER_GITHUB_API_TOKEN="5d42eadf98c58c9c4f60""7fcfc72cee4c7ef1486b" danger --dangerfile=.circleci/Dangerfile_Lib.rb --danger_id="${CURRENT_LIB}" --verbose
+            DANGER_GITHUB_API_TOKEN="d2d9579980154e199b14""ba8173aa935088d30d2a" danger --dangerfile=.circleci/Dangerfile_Lib.rb --danger_id="${CURRENT_LIB}" --verbose
         fi
     else
         echo "No need to run Danger."
