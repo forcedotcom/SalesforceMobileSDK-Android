691a7f0a6b0eb8548e96d9eef7068ad342a52372
Mike DeBlase <deblasem@gmail.com>
2018-10-03 11:50:26 -0700

    more imports

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/push/PushMessaging.java
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/push/PushMessaging.java
@@ -30,6 +30,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.JobIntentService;
 
