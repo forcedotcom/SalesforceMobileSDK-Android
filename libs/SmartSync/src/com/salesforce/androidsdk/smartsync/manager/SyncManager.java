39109c0d0a705f8909dfcdec42ee5aed922bc3fd
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-02-07 11:05:34 -0800

    Fixing imports

--- libs/SmartSync/src/com/salesforce/androidsdk/smartsync/manager/SyncManager.java
+++ libs/SmartSync/src/com/salesforce/androidsdk/smartsync/manager/SyncManager.java
@@ -54,7 +54,6 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
