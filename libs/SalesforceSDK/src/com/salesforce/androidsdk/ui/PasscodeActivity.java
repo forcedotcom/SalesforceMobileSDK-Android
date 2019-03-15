a6e3bcee26435aed6350ecb8f56deca57e5a6746
Bharath Hariharan <bhariharan@salesforce.com>
2019-03-15 14:38:23 -0700

    Removing unused import

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/ui/PasscodeActivity.java
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/ui/PasscodeActivity.java
@@ -33,7 +33,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.biometrics.BiometricPrompt;
 import android.hardware.fingerprint.FingerprintManager;
