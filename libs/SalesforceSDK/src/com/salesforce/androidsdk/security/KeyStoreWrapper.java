a2e0a69867a994f8658bd2bc26910c6e2cb77306
Bharath Hariharan <bhariharan@salesforce.com>
2019-03-22 10:33:51 -0700

    Adding StrongBox Keymaster protection for Android P

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/security/KeyStoreWrapper.java
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/security/KeyStoreWrapper.java
@@ -30,6 +30,7 @@ import android.os.Build;
 import android.security.KeyPairGeneratorSpec;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
+import android.security.keystore.StrongBoxUnavailableException;
 import android.util.Base64;
 
 import com.salesforce.androidsdk.app.SalesforceSDKManager;
@@ -228,8 +229,22 @@ public class KeyStoreWrapper {
                             KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
                             .setKeySize(length)
                             .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1);
-                    kpg.initialize(keyGenParameterSpecBuilder.build());
-                    kpg.generateKeyPair();
+
+                    /*
+                     * TODO: Remove this check once minVersion > 28.
+                     */
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        try {
+                            keyGenParameterSpecBuilder.setIsStrongBoxBacked(true);
+                            kpg.initialize(keyGenParameterSpecBuilder.build());
+                            kpg.generateKeyPair();
+                        } catch (StrongBoxUnavailableException sb) {
+                            SalesforceSDKLogger.e(TAG, "StrongBox Keymaster unavailable", sb);
+                            keyGenParameterSpecBuilder.setIsStrongBoxBacked(false);
+                            kpg.initialize(keyGenParameterSpecBuilder.build());
+                            kpg.generateKeyPair();
+                        }
+                    }
                 } else {
 
                     /*
