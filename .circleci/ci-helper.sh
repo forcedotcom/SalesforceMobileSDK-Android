2dc08aeb1a4474c389711318307701deaaddd219
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-04-06 12:39:46 -0700

    Relative path

--- .circleci/ci-helper.sh
+++ .circleci/ci-helper.sh
@@ -14,7 +14,7 @@ function envSetup {
     gem install danger-android_lint
     gem install danger-jacoco
 
-    echo $TEST_CREDENTIALS > shared/test/test_credentials.json
+    echo $TEST_CREDENTIALS > ./shared/test/test_credentials.json
 }
 
 function printTestsToRun {
