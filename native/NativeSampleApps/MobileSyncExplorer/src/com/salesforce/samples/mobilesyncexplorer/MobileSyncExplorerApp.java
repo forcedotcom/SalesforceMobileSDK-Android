e8393969afd4ddd624280b5b093586888cafaf9c
Bharath Hariharan <bhariharan@salesforce.com>
2019-10-30 18:46:07 -0700

    Fixing typos

--- native/NativeSampleApps/MobileSyncExplorer/src/com/salesforce/samples/mobilesyncexplorer/MobileSyncExplorerApp.java
+++ native/NativeSampleApps/MobileSyncExplorer/src/com/salesforce/samples/mobilesyncexplorer/MobileSyncExplorerApp.java
@@ -51,7 +51,7 @@ public class MobileSyncExplorerApp extends Application {
 		/*
 		 * Un-comment the line below to enable push notifications in this app.
 		 * Replace 'pnInterface' with your implementation of 'PushNotificationInterface'.
-		 * Add your Google package ID in 'bootonfig.xml', as the value
+		 * Add your Google package ID in 'bootconfig.xml', as the value
 		 * for the key 'androidPushNotificationClientId'.
 		 */
 		// MobileSyncSDKManager.getInstance().setPushNotificationReceiver(pnInterface);
