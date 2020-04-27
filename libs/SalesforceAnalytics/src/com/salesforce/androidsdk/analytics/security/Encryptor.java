76d388bd6e09be6d2dad7ea739f49bc51e83650e
Bharath Hariharan <bhariharan@salesforce.com>
2020-04-26 18:12:28 -0700

    First stab at adding support for AES/GCM/NoPadding

--- libs/SalesforceAnalytics/src/com/salesforce/androidsdk/analytics/security/Encryptor.java
+++ libs/SalesforceAnalytics/src/com/salesforce/androidsdk/analytics/security/Encryptor.java
@@ -52,17 +52,18 @@ import javax.crypto.spec.SecretKeySpec;
 public class Encryptor {
 
     private static final String TAG = "Encryptor";
-    private static final String PREFER_CIPHER_TRANSFORMATION = "AES/CBC/PKCS5Padding";
+    private static final String AES_CBC_CIPHER = "AES/CBC/PKCS5Padding";
+    private static final String AES_GCM_CIPHER = "AES/GCM/NoPadding";
     private static final String MAC_TRANSFORMATION = "HmacSHA256";
     private static final String SHA1PRNG = "SHA1PRNG";
     private static final String RSA_PKCS1 = "RSA/ECB/PKCS1Padding";
     private static final String BOUNCY_CASTLE = "BC";
 
     /**
-     * Return initialized cipher for encryption
-     * NB: an IV is generated
-     * @param encryptionKey encryption key
-     * @return initialized cipher
+     * Returns initialized cipher for encryption with an IV automatically generated.
+     *
+     * @param encryptionKey Encryption key.
+     * @return Initialized cipher.
      */
     public static Cipher getEncryptingCipher(String encryptionKey)
         throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
@@ -72,7 +73,7 @@ public class Encryptor {
 
     private static Cipher getEncryptingCipher(byte[] keyBytes, byte[] iv)
         throws InvalidAlgorithmParameterException, InvalidKeyException {
-        final Cipher cipher = getBestCipher();
+        final Cipher cipher = getBestCipher(AES_GCM_CIPHER);
         final SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, cipher.getAlgorithm());
         final IvParameterSpec ivSpec = new IvParameterSpec(iv);
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivSpec);
@@ -80,10 +81,11 @@ public class Encryptor {
     }
 
     /**
-     * Return initialized cipher for decryption
-     * @param encryptionKey encryption key
-     * @param iv bytes of iv
-     * @return inialized cipher
+     * Returns initialized cipher for decryption.
+     *
+     * @param encryptionKey Encryption key.
+     * @param iv Initialization vector.
+     * @return Initialized cipher.
      */
     public static Cipher getDecryptingCipher(String encryptionKey, byte[] iv)
         throws InvalidAlgorithmParameterException, InvalidKeyException {
@@ -92,8 +94,20 @@ public class Encryptor {
     }
 
     private static Cipher getDecryptingCipher(byte[] keyBytes, byte[] iv)
-        throws InvalidAlgorithmParameterException, InvalidKeyException {
-        final Cipher cipher = getBestCipher();
+            throws InvalidAlgorithmParameterException, InvalidKeyException {
+        final Cipher cipher = getBestCipher(AES_GCM_CIPHER);
+        final SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, cipher.getAlgorithm());
+        final IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivSpec);
+        return cipher;
+    }
+
+    /*
+     * TODO: Remove this in Mobile SDK 10.0.
+     */
+    private static Cipher getLegacyDecryptingCipher(byte[] keyBytes, byte[] iv)
+            throws InvalidAlgorithmParameterException, InvalidKeyException {
+        final Cipher cipher = getBestCipher(AES_CBC_CIPHER);
         final SecretKeySpec skeySpec = new SecretKeySpec(keyBytes, cipher.getAlgorithm());
         final IvParameterSpec ivSpec = new IvParameterSpec(iv);
         cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivSpec);
@@ -101,18 +115,55 @@ public class Encryptor {
     }
 
     /**
-     * Decrypts data with key using AES-128.
+     * Decrypts data with key using AES/CBC/PKCS5Padding. Should be used only for
+     * migration of encrypted data from an older version to Mobile SDK 8.2.
+     *
+     * @param data Data.
+     * @param key Base64 encoded 128 bit key or null (to leave data unchanged).
+     * @return Decrypted data.
+     * @deprecated Will be removed in Mobile SDK 10.0.
+     */
+    public static String legacyDecrypt(String data, String key) {
+        return decrypt(data, key, new byte[16]);
+    }
+
+    /**
+     * Decrypts data with key using AES/GCM/NoPadding.
      *
      * @param data Data.
      * @param key Base64 encoded 128 bit key or null (to leave data unchanged).
      * @return Decrypted data.
      */
     public static String decrypt(String data, String key) {
+        return decrypt(data, key, new byte[12]);
+    }
+
+    /**
+     * Decrypts data with key using AES/CBC/PKCS5Padding. Should be used only for
+     * migration of encrypted data from an older version to Mobile SDK 8.2.
+     *
+     * @param data Data.
+     * @param key Key.
+     * @return Decrypted data.
+     * @deprecated Will be removed in Mobile SDK 10.0.
+     */
+    public static String legacyDecrypt(byte[] data, String key) {
         return decrypt(data, key, new byte[16]);
     }
 
     /**
-     * Decrypts data with key using AES-128.
+     * Decrypts data with key using AES/GCM/NoPadding.
+     *
+     * @param data Data.
+     * @param key Key.
+     * @return Decrypted data.
+     */
+    public static String decrypt(byte[] data, String key) {
+        return decrypt(data, key, new byte[12]);
+    }
+
+    /**
+     * Decrypts data with key using AES/GCM/NoPadding.
      *
      * @param data Data.
      * @param key Base64 encoded 128 bit key or null (to leave data unchanged).
@@ -127,18 +178,7 @@ public class Encryptor {
     }
 
     /**
-     * Decrypts data with key using AES-128.
-     *
-     * @param data Data.
-     * @param key Key.
-     * @return Decrypted data.
-     */
-    public static String decrypt(byte[] data, String key) {
-        return decrypt(data, key, new byte[16]);
-    }
-
-    /**
-     * Decrypts data with key using AES-128.
+     * Decrypts data with key using using AES/GCM/NoPadding.
      *
      * @param data Data.
      * @param key Key.
@@ -159,7 +199,7 @@ public class Encryptor {
             byte[] keyBytes = Base64.decode(key, Base64.DEFAULT);
             byte[] dataBytes = Base64.decode(data, Base64.DEFAULT);
 
-            // Decrypts with AES-128.
+            // Decrypts with AES.
             byte[] decryptedData = decrypt(dataBytes, dataBytes.length, keyBytes, iv);
             return new String(decryptedData, 0, decryptedData.length, StandardCharsets.UTF_8);
         } catch (Exception ex) {
@@ -169,11 +209,11 @@ public class Encryptor {
     }
 
     /**
-     * Encrypts data with key using AES-128.
+     * Encrypts data with key using AES/GCM/NoPadding.
      *
      * @param data Data.
      * @param key Base64 encoded 128 bit key or null (to leave data unchanged).
-     * @return Base64, AES-128 encrypted data.
+     * @return Encrypted data.
      */
     public static String encrypt(String data, String key) {
         try {
@@ -185,12 +225,12 @@ public class Encryptor {
     }
 
     /**
-     * Encrypts data with key using AES-128.
+     * Encrypts data with key using AES/GCM/NoPadding.
      *
      * @param data Data.
      * @param key Base64 encoded 128 bit key or null (to leave data unchanged).
      * @param iv Initialization vector.
-     * @return Base64, AES-128 encrypted data.
+     * @return Encrypted data.
      */
     public static String encrypt(String data, String key, byte[] iv) {
         if (TextUtils.isEmpty(key) || data == null) {
@@ -211,7 +251,7 @@ public class Encryptor {
     }
 
     /**
-     * Encrypts data with key using AES-128. Returns Base64 byte[] array.
+     * Encrypts data with key using AES/GCM/NoPadding.
      *
      * @param data Data.
      * @param key Key.
@@ -227,7 +267,7 @@ public class Encryptor {
     }
 
     /**
-     * Encrypts data with key using AES-128. Returns Base64 byte[] array.
+     * Encrypts data with key using AES/GCM/NoPadding.
      *
      * @param data Data.
      * @param key Key.
@@ -372,7 +412,7 @@ public class Encryptor {
      */
     public static String decryptBytes(byte[] data, byte[] key, byte[] iv) {
         try {
-            final Cipher cipher = getDecryptingCipher(key, iv);
+            final Cipher cipher = getLegacyDecryptingCipher(key, iv);
             byte[] result = cipher.doFinal(data, 0, data.length);
             return new String(result, 0, result.length, StandardCharsets.UTF_8);
         } catch (Exception e) {
@@ -412,7 +452,7 @@ public class Encryptor {
 
     private static byte[] generateInitVector() throws NoSuchAlgorithmException {
         final SecureRandom random = SecureRandom.getInstance(SHA1PRNG);
-        byte[] iv = new byte[16];
+        byte[] iv = new byte[12];
         random.nextBytes(iv);
         return iv;
     }
@@ -421,7 +461,7 @@ public class Encryptor {
         final Cipher cipher = getEncryptingCipher(key, iv);
         byte[] meat = cipher.doFinal(data);
 
-        // Prepends the IV to the encoded data (first 16 bytes / 128 bits).
+        // Prepends the IV to the encoded data (first 12 bytes for GCM).
         byte[] result = new byte[iv.length + meat.length];
         System.arraycopy(iv, 0, result, 0, iv.length);
         System.arraycopy(meat, 0, result, iv.length, meat.length);
@@ -430,7 +470,7 @@ public class Encryptor {
 
     private static byte[] decrypt(byte[] data, int length, byte[] key, byte[] iv) throws GeneralSecurityException {
 
-        // Grabs the init vector prefix (first 16 bytes / 128 bits).
+        // Grabs the init vector prefix (first 12 bytes for GCM, or first 16 bytes for CBC).
         System.arraycopy(data, 0, iv, 0, iv.length);
 
         // Grabs the encrypted body after the init vector prefix.
@@ -438,14 +478,21 @@ public class Encryptor {
         int meatOffset = iv.length;
         byte[] meat = new byte[meatLen];
         System.arraycopy(data, meatOffset, meat, 0, meatLen);
-        final Cipher cipher = getDecryptingCipher(key, iv);
+        Cipher cipher;
+
+        // AES/GCM has an IV of length 12 bytes, whereas AES/CBC has an IV of length 16 bytes.
+        if (iv.length == 12) {
+            cipher = getDecryptingCipher(key, iv);
+        } else {
+            cipher = getLegacyDecryptingCipher(key, iv);
+        }
         return cipher.doFinal(meat, 0, meatLen);
     }
 
-    private static Cipher getBestCipher() {
+    private static Cipher getBestCipher(String cipherMode) {
         Cipher cipher = null;
         try {
-            cipher = Cipher.getInstance(PREFER_CIPHER_TRANSFORMATION, getLegacyEncryptionProvider());
+            cipher = Cipher.getInstance(cipherMode, getLegacyEncryptionProvider());
         } catch (Exception e) {
             SalesforceAnalyticsLogger.e(null, TAG,
                     "No cipher transformation available", e);
