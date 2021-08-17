8822cf82c9166822aeaa8594281a5e1e91437c36
Bharath Hariharan <bhariharan@salesforce.com>
2021-08-17 11:26:33 -0700

    Upgrading to Cordova 10.1.0

--- external/cordova/cordova.js
+++ external/cordova/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '10.0.1';
+var PLATFORM_VERSION_BUILD_LABEL = '10.1.0';
 // file: src/scripts/require.js
 var require;
 var define;
