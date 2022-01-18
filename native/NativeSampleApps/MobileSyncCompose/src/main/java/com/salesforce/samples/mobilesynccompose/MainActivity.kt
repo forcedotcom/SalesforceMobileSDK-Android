7899bc2690fb5a118745acd4d2d9bb9ccd101da4
Nikolai Kotula <gkotula@salesforce.com>
2022-01-18 11:00:43 -0700

    Removes Compose Material 3 and adds Contact Card
    Compose Material 3 was really showing its alpha-ness.  Basic functionality in
    theming was broken for some built-in Material components, so this abandons that
    pursuit until Compose Material 3 is stable.

--- native/NativeSampleApps/MobileSyncCompose/src/main/java/com/salesforce/samples/mobilesynccompose/MainActivity.kt
+++ native/NativeSampleApps/MobileSyncCompose/src/main/java/com/salesforce/samples/mobilesynccompose/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.salesforce.samples.mobilesynccompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.salesforce.samples.mobilesynccompose.ui.theme.SalesforceMobileSDKAndroidTheme
+import androidx.compose.ui.unit.dp
+import com.salesforce.samples.mobilesynccompose.contacts.ui.ContactCard
+import com.salesforce.samples.mobilesynccompose.core.ui.theme.SalesforceMobileSDKAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,24 +22,26 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Androidd aaaa")
+                    ContactCard(
+                        startExpanded = false,
+                        isSynced = true,
+                        name = "Foo Bar",
+                        title = "AHHHH"
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     SalesforceMobileSDKAndroidTheme {
-        Greeting("Android")
+        Surface(modifier = Modifier.padding(4.dp)) {
+            ContactCard(startExpanded = true, isSynced = true, name = "Foo Bar", title = "AHHH")
+        }
     }
 }
---
+++ native/NativeSampleApps/MobileSyncCompose/src/main/java/com/salesforce/samples/mobilesynccompose/core/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.salesforce.samples.mobilesynccompose.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
