997263736b6bc357c9bd884dec5661a696b75679
Brandon Page <bpage@salesforce.com>
2022-04-11 16:52:53 -0700

    Fix SalesforceSDK Tests.

--- libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/analytics/SalesforceAnalyticsManagerTest.java
+++ libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/analytics/SalesforceAnalyticsManagerTest.java
@@ -86,6 +86,7 @@ public class SalesforceAnalyticsManagerTest {
         assertEquals(3, manager.getEventStoreManager().getNumStoredEvents());
         assertEquals(1, TestPublisher.publishedEvents.size());
         assertEquals(2, TestPublisher.publishedEvents.get(0).length());
+        manager.getEventStoreManager().deleteAllEvents();
     }
 
     private void storeEvents(int count) {
