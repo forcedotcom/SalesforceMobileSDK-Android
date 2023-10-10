2f920f435d841d490de1f311ba5cde75b6f08a51
Eric C. Johnson <Johnson.Eric@Salesforce.com>
2023-10-10 16:17:42 -0600

    @W-14246983: [MSDK Android Templates] Templates Use Maven Central MSDK Dependencies By Default˜

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,23 @@
+val nodeModulesMsdk = File("${rootProject.projectDir}/libs/SalesforceReact/node_modules/react-native/android") // For stand-alone MSDK builds.
+val nodeModulesTemplate = File("${rootProject.projectDir}/../../node_modules/react-native/android") // For template app builds.
+val hasNodeModules = nodeModulesMsdk.exists() || nodeModulesTemplate.exists()
+
 include("libs:SalesforceAnalytics")
 include("libs:SalesforceSDK")
 include("libs:SmartStore")
 include("libs:MobileSync")
 include("libs:SalesforceHybrid")
-include("libs:SalesforceReact")
-include("native:NativeSampleApps:RestExplorer")
-include("native:NativeSampleApps:MobileSyncExplorer")
+if (hasNodeModules) {
+    include("libs:SalesforceReact")
+} else {
+    logger.warn("Skipping `SalesforceReact` module since local node repository is missing.  Has `yarn install` been run in the directory containing `package.json`?")
+}
+include("hybrid:HybridSampleApps:AccountEditor")
 include("native:NativeSampleApps:AppConfigurator")
 include("native:NativeSampleApps:ConfiguredApp")
-include("hybrid:HybridSampleApps:AccountEditor")
+include("native:NativeSampleApps:MobileSyncExplorer")
 include("hybrid:HybridSampleApps:MobileSyncExplorerHybrid")
+include("native:NativeSampleApps:RestExplorer")
 
 dependencyResolutionManagement {
     repositories {
