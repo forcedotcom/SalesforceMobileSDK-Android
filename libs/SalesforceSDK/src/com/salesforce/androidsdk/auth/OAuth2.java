efeb18c22ccb62d6055b084618d994af295b5343
Wolfgang Mathurin <wmathurin@salesforce.com>
2024-03-13 15:03:20 -0700

    Adding missing field to IdServiceResponse

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/auth/OAuth2.java
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/auth/OAuth2.java
@@ -109,7 +109,6 @@ public class OAuth2 {
     private static final String PHOTOS = "photos";
     private static final String PICTURE = "picture";
     private static final String THUMBNAIL = "thumbnail";
-
     private static final String AUTHORIZATION_CODE = "authorization_code";
     private static final String HYBRID_AUTH_CODE = "hybrid_auth_code";
     private static final String CODE = "code";
@@ -146,6 +145,32 @@ public class OAuth2 {
     private static final String FORWARD_SLASH = "/";
     private static final String SINGLE_SPACE = " ";
     private static final String TAG = "OAuth2";
+    private static final String ID_URL = "id";
+    private static final String ASSERTED_USER = "asserted_user";
+    private static final String USER_ID = "user_id";
+    private static final String ORG_ID = "organization_id";
+    private static final String NICKNAME = "nick_name";
+    private static final String URLS = "urls";
+    private static final String ENTERPRISE_SOAP_URL = "enterprise";
+    private static final String METADATA_SOAP_URL = "metadata";
+    private static final String PARTNER_SOAP_URL = "partner";
+    private static final String REST_URL = "rest";
+    private static final String REST_SOBJECTS_URL = "sobjects";
+    private static final String REST_SEARCH_URL = "search";
+    private static final String REST_QUERY_URL = "query";
+    private static final String REST_RECENT_URL = "recent";
+    private static final String PROFILE_URL = "profile";
+    private static final String CHATTER_FEEDS_URL = "feeds";
+    private static final String CHATTER_GROUPS_URL = "groups";
+    private static final String CHATTER_USERS_URL = "users";
+    private static final String CHATTER_FEED_ITEMS_URL = "feed_items";
+    private static final String IS_ACTIVE = "active";
+    private static final String USER_TYPE = "user_type";
+    private static final String LANGUAGE = "language";
+    private static final String LOCALE = "locale";
+    private static final String UTC_OFFSET = "utcOffset";
+    private static final String MOBILE_APP_PIN_LENGTH = "pin_length";
+    private static final String LAST_MODIFIED_DATE = "last_modified_date";
 
     /**
      * Builds the URL to the authorization web page for this login server.
@@ -497,6 +522,35 @@ public class OAuth2 {
         public JSONObject customAttributes;
         public JSONObject customPermissions;
 
+        public String idUrl;
+        public boolean assertedUser;
+        public String userId;
+        public String orgId;
+        public String nickname;
+        public String photos;
+        public String urls;
+        public String enterpriseSoapUrl;
+        public String metadataSoapUr;
+        public String partnerSoapUrl;
+        public String restUrl;
+        public String restSObjectsUrl;
+        public String restSearchUrl;
+        public String restQueryUrl;
+        public String restRecentUrl;
+        public String profileUrl;
+        public String chatterFeedsUrl;
+        public String chatterGroupsUrl;
+        public String chatterUsersUrl;
+        public String chatterFeedItemsUrl;
+        public boolean isActive;
+        public String userType;
+        public String language;
+        public String locale;
+        public int utcOffset;
+        public boolean mobilePolicyConfigured;
+        public String mobileAppPinLength;
+        public String lastModifiedDate;
+
         /**
          * Parameterized constructor built from identity service response.
          *
@@ -515,6 +569,37 @@ public class OAuth2 {
                     pictureUrl = photos.getString(PICTURE);
                     thumbnailUrl = photos.getString(THUMBNAIL);
                 }
+
+                idUrl = parsedResponse.getString(ID_URL);
+                assertedUser = parsedResponse.optBoolean(ASSERTED_USER);
+                userId = parsedResponse.getString(USER_ID);
+                orgId = parsedResponse.getString(ORG_ID);
+                nickname = parsedResponse.getString(NICKNAME);
+                final JSONObject urls = parsedResponse.getJSONObject(URLS);
+                if (urls != null) {
+                    enterpriseSoapUrl = urls.getString(ENTERPRISE_SOAP_URL);
+                    metadataSoapUr = urls.getString(METADATA_SOAP_URL);
+                    partnerSoapUrl = urls.getString(PARTNER_SOAP_URL);
+                    restUrl = urls.getString(REST_URL);
+                    restSObjectsUrl = urls.getString(REST_SOBJECTS_URL);
+                    restSearchUrl = urls.getString(REST_SEARCH_URL);
+                    restQueryUrl = urls.getString(REST_QUERY_URL);
+                    restRecentUrl = urls.getString(REST_RECENT_URL);
+                    profileUrl = urls.getString(PROFILE_URL);
+                    chatterFeedsUrl = urls.getString(CHATTER_FEEDS_URL);
+                    chatterGroupsUrl = urls.getString(CHATTER_GROUPS_URL);
+                    chatterUsersUrl = urls.getString(CHATTER_USERS_URL);
+                    chatterFeedItemsUrl = urls.getString(CHATTER_FEED_ITEMS_URL);
+                }
+                isActive = parsedResponse.optBoolean(IS_ACTIVE);
+                userType = parsedResponse.getString(USER_TYPE);
+                language = parsedResponse.getString(LANGUAGE);
+                locale = parsedResponse.getString(LOCALE);
+                utcOffset = parsedResponse.optInt(UTC_OFFSET, -1);
+                mobilePolicyConfigured = parsedResponse.has(MOBILE_POLICY);
+                mobileAppPinLength = parsedResponse.getString(MOBILE_APP_PIN_LENGTH);
+                lastModifiedDate = parsedResponse.getString(LAST_MODIFIED_DATE);
+
                 customAttributes = parsedResponse.optJSONObject(CUSTOM_ATTRIBUTES);
                 customPermissions = parsedResponse.optJSONObject(CUSTOM_PERMISSIONS);
 
@@ -531,7 +616,7 @@ public class OAuth2 {
                 }
 
 
-                if (parsedResponse.has(MOBILE_POLICY)) {
+                if (mobilePolicyConfigured) {
                     JSONObject mobilePolicyObject = parsedResponse.getJSONObject(MOBILE_POLICY);
                     screenLock = mobilePolicyObject.has(SCREEN_LOCK_TIMEOUT);
                     screenLockTimeout = mobilePolicyObject.getInt(SCREEN_LOCK_TIMEOUT);
