b6cafa4a894432f707d4cd9fc0a23b073d566696
Wolfgang Mathurin <wmathurin@salesforce.com>
2024-03-20 13:47:55 -0700

    Moving to React Native 0.73.6

--- libs/SalesforceReact/build.gradle.kts
+++ libs/SalesforceReact/build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
 
 dependencies {
     api(project(":libs:MobileSync"))
-    api("com.facebook.react:react-android:0.72.7")
+    api("com.facebook.react:react-android:0.73.6")
     implementation("androidx.core:core-ktx:1.12.0")
     androidTestImplementation("androidx.test:runner:1.5.2")
     androidTestImplementation("androidx.test:rules:1.5.0")
