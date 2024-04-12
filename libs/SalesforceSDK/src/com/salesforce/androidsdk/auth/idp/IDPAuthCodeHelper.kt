440ac67f93da2ddf888bc31bf0bd02e433104238
Brandon Page <bpage@salesforce.com>
2024-04-12 13:50:01 -0700

    Maybe userHybridAuthentication a settable variable.

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/auth/idp/IDPAuthCodeHelper.kt
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/auth/idp/IDPAuthCodeHelper.kt
@@ -142,7 +142,7 @@ internal class IDPAuthCodeHelper private constructor(
     fun makeFrontDoorRequest(accessToken: String, webView: WebView) {
         SalesforceSDKLogger.d(TAG, "Making front door request")
         val context = SalesforceSDKManager.getInstance().appContext
-        val useHybridAuthentication = SalesforceSDKManager.getInstance().shouldUseHybridAuthentication()
+        val useHybridAuthentication = SalesforceSDKManager.getInstance().useHybridAuthentication
         val frontdoorUrl = getFrontdoorUrl(
             getAuthorizationUrl(
                 true, // use web server flow
