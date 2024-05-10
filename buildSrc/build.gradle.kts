1c88cfbe5b18d62c069e537f94ce6c4720c75376
Mahipal Singh <mahipal.singh-1@syngenta.com>
2024-05-10 16:03:29 +0530

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.2.0")
+    implementation("com.android.tools.build:gradle:8.2.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
 }
