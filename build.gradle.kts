790e6184bcf69693773f924259b449d6e3464332
Wolfgang Mathurin <wmathurin@salesforce.com>
2024-08-29 10:43:16 -0700

    Updating version numbers to 12.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
 
 allprojects {
     group = "com.salesforce.mobilesdk"
-    version = "12.1.1"
+    version = "12.2.0"
 
     // Ensure that we do not use newer language features that would make the SDK incompatible with
     // apps that do not target the latest version of Kotlin.
