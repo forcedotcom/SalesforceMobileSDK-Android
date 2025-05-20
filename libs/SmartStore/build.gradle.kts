ca73a774d6e726107bfb71b37d44f3a21e2a7a1b
Wolfgang Mathurin <wmathurin@salesforce.com>
2025-05-19 17:53:37 -0700

    Moving to SQLCipher 4.9.0

--- libs/SmartStore/build.gradle.kts
+++ libs/SmartStore/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     api(project(":libs:SalesforceSDK"))
     //noinspection GradleDependency -  Needs to line up with supported SQLCipher version.
     api("androidx.sqlite:sqlite:2.2.0")
-    api("net.zetetic:sqlcipher-android:4.7.2")
+    api("net.zetetic:sqlcipher-android:4.9.0")
     implementation("androidx.core:core-ktx:1.15.0")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test:rules:1.6.1")
