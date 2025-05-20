72e010867335a100a2ef680089bc15014b1f34db
Eric C. Johnson <Johnson.Eric@Salesforce.com>
2025-05-19 19:27:32 -0600

    @W-17449395: [Android] Welcome login email deeplinking support in MSDK 13.1 (SalesforceSDKManagerTests.expectedValue_Returns_onFetchAuthenticationConfiguration - Fixes Pt. 2)

--- libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/app/SalesforceSDKManagerTests.kt
+++ libs/test/SalesforceSDKTest/src/com/salesforce/androidsdk/app/SalesforceSDKManagerTests.kt
@@ -54,6 +54,29 @@ class SalesforceSDKManagerTests {
         var authConfig: MyDomainAuthConfig? = null
 
 
+        // Login Server: "My Domain"/Other URL
+        SalesforceSDKManager.getInstance().loginServerManager.setSelectedLoginServer(
+            LoginServer(
+                "Example",
+                "https://www.example.com",
+                true
+            )
+        )
+
+        runBlocking {
+            SalesforceSDKManager.getInstance().fetchAuthenticationConfiguration(
+                httpAccess = httpAccess,
+            ) {
+                authConfig = MyDomainAuthConfig((RestResponse(response)).asJSONObject())
+            }.join()
+        }
+
+        assertEquals(
+            "{\"id\":\"https:\\/\\/login.salesforce.com\\/id\\/00DB0000000ToZ3MAK\\/005B0000009mWwmIAE\",\"asserted_user\":true,\"user_id\":\"005B0000009mWwmIAE\",\"organization_id\":\"00DB0000000ToZ3MAK\",\"username\":\"johnson.eric@gs0.mobilesdk.com\",\"nick_name\":\"ejohnson\",\"display_name\":\"Eric Johnson\",\"email\":\"johnson.eric@salesforce.com\",\"email_verified\":true,\"first_name\":\"Eric\",\"last_name\":\"Johnson\",\"timezone\":\"America\\/Los_Angeles\",\"photos\":{\"picture\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/profilephoto\\/7291Q0000001oer\\/F\",\"thumbnail\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/profilephoto\\/7291Q0000001oer\\/T\"},\"addr_street\":null,\"addr_city\":null,\"addr_state\":null,\"addr_country\":null,\"addr_zip\":null,\"mobile_phone\":null,\"mobile_phone_verified\":true,\"is_lightning_login_user\":false,\"status\":{\"created_date\":null,\"body\":null},\"urls\":{\"enterprise\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/Soap\\/c\\/{version}\\/00DB0000000ToZ3\",\"metadata\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/Soap\\/m\\/{version}\\/00DB0000000ToZ3\",\"partner\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/Soap\\/u\\/{version}\\/00DB0000000ToZ3\",\"rest\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/\",\"sobjects\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/sobjects\\/\",\"search\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/search\\/\",\"query\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/query\\/\",\"recent\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/recent\\/\",\"tooling_soap\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/Soap\\/T\\/{version}\\/00DB0000000ToZ3\",\"tooling_rest\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/tooling\\/\",\"profile\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/005B0000009mWwmIAE\",\"feeds\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/chatter\\/feeds\",\"groups\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/chatter\\/groups\",\"users\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/chatter\\/users\",\"feed_items\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/chatter\\/feed-items\",\"feed_elements\":\"https:\\/\\/mobilesdk.my.salesforce.com\\/services\\/data\\/v{version}\\/chatter\\/feed-elements\",\"custom_domain\":\"https:\\/\\/mobilesdk.my.salesforce.com\"},\"active\":true,\"user_type\":\"STANDARD\",\"language\":\"en_US\",\"locale\":\"en_US\",\"utcOffset\":-28800000,\"last_modified_date\":\"2025-02-28T18:14:06Z\"}",
+            authConfig?.authConfig?.toString()
+        )
+
+
         // Login Server: Welcome
         SalesforceSDKManager.getInstance().loginServerManager.setSelectedLoginServer(
             LoginServer(
