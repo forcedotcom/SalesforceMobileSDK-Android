a5b41dee7304e0272795de7bcab587b1f5c9f375
Wolfgang Mathurin <wmathurin@salesforce.com>
2025-05-22 07:38:01 -0700

    Mobile SDK 13.0.1

--- libs/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.kt
+++ libs/SalesforceSDK/src/com/salesforce/androidsdk/app/SalesforceSDKManager.kt
@@ -1557,7 +1557,7 @@ open class SalesforceSDKManager protected constructor(
         protected var INSTANCE: SalesforceSDKManager? = null
 
         /** The current version of this SDK */
-        const val SDK_VERSION = "13.0.0"
+        const val SDK_VERSION = "13.0.1"
 
         /**
          * An intent action meant for instances of Salesforce SDK manager
