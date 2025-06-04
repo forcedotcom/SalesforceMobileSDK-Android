b62ea08ec4748791ccc0a22b1da96834ccd117ce
Eric C. Johnson <Johnson.Eric@Salesforce.com>
2025-06-04 10:53:41 -0600

    @W-16556387: [ReactNative][Android] Upgrade to React Native 0.79 (W.I.P. w/Syntax Error In `npx react-native start`ˆ)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.6.1")
+    implementation("com.android.tools.build:gradle:8.10.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
 }
--- libs/SalesforceReact/babel.config.js
+++ libs/SalesforceReact/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['@react-native/babel-preset'],
     plugins: [
 	["@babel/plugin-transform-private-methods", { "loose": true }]
     ]    
