862f049c1cace1bd22881150f7a465e59946eb69
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-04-02 10:33:56 -0700

    Updating version numbers to 14.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
 
 allprojects {
     group = "com.salesforce.mobilesdk"
-    version = "13.2.0"
+    version = "14.0.0"
 
     // Ensure that we do not use newer language features that would make the SDK incompatible with
     // apps that do not target the latest version of Kotlin.
