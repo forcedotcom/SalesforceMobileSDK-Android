570af8b984b80b85462e3c85dac1555f3e53b3a5
Eric C. Johnson <Johnson.Eric@Salesforce.com>
2026-04-23 13:20:52 -0600

    @W-21933885: [MSDK Android] App Attestation Implementation (Migrate To Toolchain)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
